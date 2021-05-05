6b1183f51d7b117e64a37e6ab7eb4cbb69f7208e
Kondratieva Anzhelika <kondrateva.anzhelika92@gmail.com>
2021-05-05 23:03:39 +0300

    Some fixes from 2.2 because of revert

--- app/etc/config.php
+++ app/etc/config.php
@@ -331,6 +331,7 @@ return [
         'Training_RedirectToMainPage' => 1,
         'Training_Routing' => 1,
         'Training_Template' => 1,
+        'Training_Test' => 1,
         'Training_TestOM' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidationApi' => 1,

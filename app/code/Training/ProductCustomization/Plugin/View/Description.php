3ca2c4599d41959f2320144fe885738a84f00a9d
Kondratieva Anzhelika <kondrateva.anzhelika92@gmail.com>
2021-05-17 00:17:36 +0300

    3.4 Product Description customization

---
+++ app/code/Training/ProductCustomization/Plugin/View/Description.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Training\ProductCustomization\Plugin\View;
+
+
+class Description
+{
+
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+
+        $subject->getProduct()->setDescription('Test description');
+
+        return [null];
+    }
+}
+

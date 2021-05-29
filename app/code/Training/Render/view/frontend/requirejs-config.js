ba29ce48743c13065c4be2817e4c80a7d5f07262
Kondratieva Anzhelika <kondrateva.anzhelika92@gmail.com>
2021-05-29 17:32:22 +0300

    4.3 New checkbox to checkout page

--- app/code/Training/Render/view/frontend/requirejs-config.js
+++ app/code/Training/Render/view/frontend/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_Catalog/js/catalog-add-to-cart': {
                 'Training_Render/js/catalog-add-to-cart/mixin': true
+            },
+            'Magento_Checkout/js/action/place-order': {
+                'Training_Render/js/checkout/action/place-order/mixin': false
             }
         }
     }
